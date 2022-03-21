e7eb64bbb681f461488b3337aa2d02dad0159927
deborahhoeltje <deborah.hoeltje@gmail.com>
2022-03-21 19:59:19 +0100

    DEA R script

---
+++ DEA.r
@@ -0,0 +1,33 @@
+library(Biobase)
+library(limma)
+
+data<- read.table(file = "/Users/deborahhoeltje/Desktop/TUBS4/medDaten/Prüfungsprojekte/Project8_crohns.txt", header = TRUE)
+
+data_control <- read.table(file = "/Users/deborahhoeltje/Desktop/TUBS4/medDaten/Prüfungsprojekte/Project8_control.txt", header = TRUE)
+
+groups = gsub("_.*", "", colnames( total_data))
+colnames(total_data)= c('hgnc', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
+
+groups = colnames(total_data)
+groups <- factor(groups, levels = c(0,1))
+
+design <- model.matrix( ~ 0 + groups )
+
+fit <- lmFit(total_data, design)
+head(coef(fit))
+dim(coef(fit))
+coef_matrix <- coef(fit)
+bayes_fit <- eBayes(fit)
+head(bayes_fit)
+head(bayes_fit$t, 3)
+
+results <- decideTests(bayes_fit[, 1])
+contmatrix = makeContrasts(groups0 - groups1, levels=colnames(design))
+fit2 <- contrasts.fit(fit, contmatrix)
+fit2
+fit2 <- eBayes(fit2)
+summary(decideTests(fit2))
+
+top.table <- topTable(fit2, sort.by = "P", n = Inf)
+head(top.table, 20)
+
