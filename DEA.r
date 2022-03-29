75446ed683aa327b27b7e26877d2395c776ca80c
deborahhoeltje <deborah.hoeltje@gmail.com>
2022-03-29 11:50:04 +0200

    exploratory analysis

--- DEA.r
+++ DEA.r
@@ -5,12 +5,16 @@ data <- read.table(file = "/Users/deborahhoeltje/Desktop/TUBS4/medDaten/Prüfun
 
 data_control <- read.table(file = "/Users/deborahhoeltje/Desktop/TUBS4/medDaten/Prüfungsprojekte/Project8_control.txt", header = TRUE)
 
+total_data <- data.frame(data, data_control)
+
 #############
 ### DEA  ###
 #############
 
-groups = gsub("_.*", "", colnames( total_data))
-colnames(total_data)= c('hgnc', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
+groups = gsub("_.*", "", colnames(total_data))
+
+# 0 for MD patients and 1 for control group
+colnames(total_data)= c('hgnc', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
 
 groups = colnames(total_data)
 groups <- factor(groups, levels = c(0,1))
@@ -29,7 +33,7 @@ head(bayes_fit)
 head(bayes_fit$t, 3)
 results <- decideTests(bayes_fit[, 1])
 
-# Test DE between group0 (MC patients) and group1 (control)
+# Test DE between group0 (MD patients) and group1 (control)
 contmatrix = makeContrasts(groups0 - groups1, levels=colnames(design))
 fit2 <- contrasts.fit(fit, contmatrix)
 fit2
@@ -49,6 +53,9 @@ summary(decideTests(fit2))
 # Gibt die top 20 differentially expressed Genes an 
 top.table <- topTable(fit2, sort.by = "P", n = Inf)
 head(top.table, 20) # see topGenes_DEA.txt file for output
+
+top.table_fc <- topTable(fit2, sort.by = "logFC", n = Inf)
+head(top.table_fc, 20)
 
 
 #############
@@ -97,7 +104,7 @@ gene_list = sort(gene_list, decreasing = TRUE)
 gse <- gseGO(geneList=gene_list, 
              ont ="ALL", 
              keyType = "SYMBOL", 
-             nPerm = 10000, 
+             #nPerm = 10000, 
              minGSSize = 3, 
              maxGSSize = 800, 
              pvalueCutoff = 0.05, 
@@ -126,7 +133,7 @@ ridgeplot(gse) + labs(x = "enrichment distribution")
 # GSEAplot, index ist das jeweilige Gen in der GESA
 gseaplot(gse, by = "all", title = gse$Description[2], geneSetID = 2)
 
-pmcplot(terms, 2010:2022, proportion=FALSE)
+pmcplot(terms, 2010:2021, proportion=FALSE)
 
 
 #######################
@@ -143,7 +150,7 @@ colnames(entrez2) <- 'entrez'
 fit3 <- fit2
 fit3$genes <- entrez2
 
-# kegga Pathway analysis
+# KEGG Pathway analysis (kegga)
 entrez_kegga = fit3$genes[, "entrez"]
 enrich_kegg <- kegga(fit3, geneid = entrez_kegga, species = "Hs")
 topKEGG(enrich_kegg)
@@ -166,10 +173,81 @@ ggo <- groupGO(gene     = de,
                 level    = 3,
                 keyType="SYMBOL",
                readable = FALSE)
-            
+
+# KEGG gseKegg
+ids<-bitr(names(original_gene_list), fromType = "SYMBOL", toType = "ENTREZID", OrgDb="org.Hs.eg.db")
+dedup_ids = ids[!duplicated(ids[c("SYMBOL")]),]
+df2 = top.table[total_data$hgnc %in% dedup_ids$SYMBOL,]
+df2$Y = dedup_ids$ENTREZID
+kegg_gene_list <- df2$logFC
+names(kegg_gene_list) <- df2$Y
+kegg_gene_list<-na.omit(kegg_gene_list)
+kegg_gene_list = sort(kegg_gene_list, decreasing = TRUE)
+kegg_organism = "hsa" # homo sapiens
+
+kk2 <- gseKEGG(geneList     = kegg_gene_list,
+               organism     = kegg_organism,
+               minGSSize    = 3,
+               maxGSSize    = 800,
+               pvalueCutoff = 0.05,
+               pAdjustMethod = "none",
+               keyType       = "ncbi-geneid")
+
+kk2@result[["ID"]]
+
+#Plots
+dotplot(kk2, showCategory = 5, title = "Enriched Pathways" , split=".sign") + facet_grid(.~.sign)
+kk_new <- pairwise_termsim(kk2, semData="org.Hs.eg.db")
+emapplot(kk_new)
+cnetplot(kk2, categorySize="pvalue", foldChange=gene_list)
+ridgeplot(kk2) + labs(x = "enrichment distribution")
+out_kk2 <- as.matrix(kk2@result)
+
+# Plot Pathways in graphics
+library(pathview)
+# take pathways form kegga_pathway.txt
+dme <- pathview(gene.data=kegg_gene_list, pathway.id="hsa02010", species = kegg_organism)
 
 # TODO: 
 # - Fishers t test to obtain p-values
 # - overrepresentation analysis: https://yulab-smu.top/biomedical-knowledge-mining-book/enrichment-overview.html
 # - gsea top table bekommen
+
+###########################
+### Explorative Analyse ###
+###########################
+
+# define important genes by calculating mean/median btw sick/not sick and chose those where the difference is significant
+# clustering might identify which genes belong to one group (pathway analysis and stuff?)
+
+total_data_matrix <- data.matrix(total_data)
+total_data_matrix <- total_data_matrix[,-c(1)]
+plotDensities(total_data_matrix)
+
+# MA Plot
+# Points further from M=0 line indicate genes with significant expression - down/upregulated
+limma::plotMA(total_data_matrix, main="MA Plot")
+
+#Hierarchical cluster (all genes would be too much), choose the ones that have the highest difference between the median of ibd patients & control group
+median_both <- data.frame(ibd = ibd_median, control=control_median)
+median_both$hgnc <- data$hgnc
+median_both$diff <- median_both$ibd - median_both$control
+high_genes <- median_both
+# high_genes[high_genes$diff >= 1,]
+
+# Filtered 124 genes to continue clustering
+filtered_genes <- high_genes[abs(high_genes$diff) >= 1,]
+
+data_filtered = data.frame()
+data_filtered <- total_data[total_data$hgnc %in% filtered_genes$hgnc,]
+
+data_filtered_dist_genes <- dist(data_filtered)
+filtered_matrix <- data.matrix(data_filtered)
+filtered_matrix <- filtered_matrix[,-c(1)]
+data_filtered_dist_samples <- dist(t(filtered_matrix))
+
+gene_hclust <- hclust(data_filtered_dist_genes, method = "complete")
+plot(gene_hclust)
+
+cutree(gene_hclust, k = 2)
 
